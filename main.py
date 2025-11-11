6fdb7a3002b66fda311ba89ce253a33100a23313
juicybob123 <okruh2028@kehillahstudent.org>
2025-11-11 09:33:32 -0800

    dddd

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-"""
-Options:
-- check the balance: prints current balance
-- withdraw money:
-    ask you how much to withdraw
-    reduce the balance by that amount
-    if you try to withdraw more than you have...
-        print error don't update the balance
-    don't withdraw a negative amount
-- deposit money:
-    ask you how to deposit
-    increase the balance by that amount
-    don't deposit a negative amount
-- loop (with a while loop) until the user says "exit"
-"""
-
-# start with 1 million dollars
-balance = 1000000
-
+#we start with 1,000,000 dollars
+y = 2
+x = 1
+while y == 2:
+    who = int(input("please input 1 if you wanna play as bill gates, input 2 if you want to play as yoni, or 3 if you wanna play as default"))
+    if who == 1:
+        balance = 103000000000
+        print("you successfully are playing as bill gates")
+        y = 3
+    elif who == 2:
+        balance = 12
+        print("you successfully are playing as yoni")
+        y = 3
+    elif who == 3:
+        balance = 1000000
+        print("you successfully are playing as default")
+        y = 3
+    else:
+        print("please pick 1, 2, or 3.")
+while x >= 0.5:     
+    promt = (input("pick From either withdraw, check your balance, or deposit money\n"))
+    if promt == ("withdraw"):
+        withdrawing = int(input("please input the amount of money you want to withdraw\n"))
+        if withdrawing <= balance and withdrawing >= 1:
+            balance = balance - withdrawing
+            print(f"your new balance is {balance}")
+        else:
+            print("you will go to jail for fraud")
+    if promt == ("balance"):
+        print (f"your balance is {balance}")
+    if promt == ("deposit"):
+        promqt = int(input("please enter the amount of money you want to deposit\n"))
+        if promqt >= 1:
+            balance = balance + promqt
+            print(f"success. Your new balance is {balance}")
+        else:
+            print("you dirty fraud trying to scam the system")
